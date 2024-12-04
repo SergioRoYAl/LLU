7598edbd194cc5824553a9f71f93d728a82c2edd
SergioRoYAl <a026577592b@ausiasmarch.net>
2024-12-04 17:40:26 +0100

    Update

--- src/main/java/web/app/api/MediaControllerApi.java
+++ src/main/java/web/app/api/MediaControllerApi.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -38,27 +39,27 @@ public class MediaControllerApi {
     @PostMapping("upload")
     public Map<String, String> uploadFile(@RequestParam("file") MultipartFile multipartFile) {
         String path = storageService.store(multipartFile);
-        String host = oHttpServletRequest.getRequestURL().toString().replace(oHttpServletRequest.getRequestURI(), "");
-       // String url = ServletUriComponentsBuilder.fromHttpUrl(host).path("/media/").path(path).toUriString();
-        String url = path;
+        String url = ServletUriComponentsBuilder.fromCurrentContextPath()
+                                               .path("/media/")
+                                               .path(path)
+                                               .toUriString();
         return Map.of("url", url);
     }
-
-    @GetMapping("{filename:.*}")
-    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
     
-        Resource file = storageService.loadAsResource(filename);
-        String contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
 
-        if (file.getFilename() != null) {
-            // Intentar obtener el tipo de contenido desde el nombre del archivo
-            contentType = URLConnection.guessContentTypeFromName(file.getFilename());
-        }
+    @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST}, maxAge = 3600)
+@GetMapping("{filename:.*}")
+public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+    Resource file = storageService.loadAsResource(filename);
+    String contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
 
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_TYPE, contentType)
-                .body(file);
-    
+    if (file.getFilename() != null) {
+        contentType = URLConnection.guessContentTypeFromName(file.getFilename());
+    }
+
+    return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_TYPE, contentType)
+            .body(file);
 }
 
 }
--- src/main/java/web/app/config/CorsConfig.java
+++ src/main/java/web/app/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                     .allowedOrigins("http://localhost:8383")
+                    .allowedOrigins("https://lluiment.es")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowedHeaders("*");
             }
